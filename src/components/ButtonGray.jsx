3dc47d0cbc77d128ecf59602f055bea09a92c6b6
JoaoFigueired0 <vicentejoao@icloud.com>
2025-04-24 00:00:11 -0300

    Atualização mov para a esquerda

--- src/components/ButtonGray.jsx
+++ src/components/ButtonGray.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 function ButtonGray(props) {
-    return (
-      <div>
-        <button className="flex rounded-[5px] px-[10px] py-[1px] border 1 border-[#FFFFFF] text-center relative left-[3px]">{props.name}</button>
-      </div>
-    );
-  }
-  
-  export default ButtonGray;
+  return (
+    <div>
+      <button
+        className="flex rounded-[5px] px-[10px] py-[1px] border 1 border-[#FFFFFF] text-center"
+        onClick={props.onClick}
+      >
+        {props.name}
+      </button>
+    </div>
+  );
+}
+
+export default ButtonGray;
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 function Input(props) {
     return (
         <div className="flex flex-col w-[250px] ml-[5%]">
